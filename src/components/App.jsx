c0056e0f94065fb21ec631c37c7f6e5fe913f810
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-09-22 14:46:47 +0300

    refactoring to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import ImageGallery from "components/ImageGallery";
 import Searchbar from "components/Searchbar";
@@ -12,95 +12,171 @@ import { Main } from "./App.styled";
 
 const IMAGES_PER_PAGE = 12;
 
-class App extends Component {
-  state = {
-    images: [],         //
-    query: '',          // query string
-    page: 1,            // current page number
-    isLastPage: false,
-    isLoading: false,   // show/hide Loader
-    modalImage: '',
-  };
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLastPage, setIsLastPage] = useState(false);  
+  const [isLoading, setIsLoading] = useState(false);  
+  const [modalImage, setModalImage] = useState('');
 
-  componentDidUpdate(_, prevState) {
-    const { query, page } = this.state;
-
-    // 
-    if (
-      prevState.query !== query ||
-      prevState.page !== page) {
-      
-      // activate preLoader;
-      this.setState({ isLoading: true });
-
-      // get images from server
-      getImage({ query, page, per_page: IMAGES_PER_PAGE })
-        .then(({ hits, totalHits }) => {
-          this.setState(prevState => ({
-            images: [
-              ...prevState.images,
-              ...hits.map(
-                ({ id, webformatURL, largeImageURL }) =>
+  // 
+  useEffect(() => {
+    if (!query) return;
+    
+    // activate preLoader;
+    setIsLoading(true);
+  
+    // get images from server
+    getImage({ query, page, per_page: IMAGES_PER_PAGE })
+      .then(({ hits, totalHits }) => {
+        
+        setImages(prevImages =>
+          [
+            ...prevImages,
+            ...hits.map(
+              ({ id, webformatURL, largeImageURL }) =>
                 ({ id, webformatURL, largeImageURL })
-              )
-            ],
-            isLastPage: (page * IMAGES_PER_PAGE >= totalHits),
-          }));
-        })
-        .catch(error => console.log(error))
-        .finally(() =>  // de-activate preLoader;
-          this.setState({ isLoading: false, })
-        );
-    };
-  };
+            )
+          ]);
+        setIsLastPage(page * IMAGES_PER_PAGE >= totalHits);
+      })
+      .catch(error => console.log(error))
+      .finally(() =>  // de-activate preLoader;
+        setIsLoading(false)
+      );
+  }, [page, query]);
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
     const { value } = e.currentTarget.query;
 
-    const query = value.trim().toLowerCase();;
-    if (!query || query === this.state.query) {
+    const queryNormalized = value.trim().toLowerCase();;
+    if (!queryNormalized || queryNormalized === query) {
       return;
     };
 
-    this.setState({ query, images: [], page: 1,});
+    setQuery(queryNormalized);
+    setImages([]);
+    setPage(1);
   };
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }))
-  };
+  const handleLoadMore = () =>
+    setPage(prevPage => prevPage + 1);
 
-  toggleModal = image =>
-    this.setState({
-      modalImage: image ?? '',
-    });
+  const toggleModal = image =>
+    setModalImage(image ?? '');
+  
+  return (
+    <Main>
+      <Searchbar onSubmit={handleSubmit} />
 
-  render() {
-    const { images, isLoading, isLastPage, modalImage } = this.state;
-    
-    return (
-      <Main>
-        <Searchbar onSubmit={this.handleSubmit} />
-
-        {!!images.length &&
-          <ImageGallery images={images} toggleModal={this.toggleModal} />}
-        
-        {isLoading && <Loader />}
-        
-        {!!images.length &&
-          !isLoading &&
-          !isLastPage &&
-          <Button onLoadMore={this.handleLoadMore} />}
-        
-        {modalImage &&
-          <Modal
-            image={modalImage}
-            toggleModal={this.toggleModal} />}
-      </Main>
-    );
-  }
+      {!!images.length &&
+        <ImageGallery images={images} toggleModal={toggleModal} />}
+      
+      {isLoading && <Loader />}
+      
+      {!!images.length &&
+        !isLoading &&
+        !isLastPage &&
+        <Button onLoadMore={handleLoadMore} />}
+      
+      {modalImage &&
+        <Modal
+          image={modalImage}
+          toggleModal={toggleModal} />}
+    </Main>
+  );
 };
+// class oldApp extends Component {
+//   state = {
+//     images: [],         //
+//     query: '',          // query string
+//     page: 1,            // current page number
+//     isLastPage: false,
+//     isLoading: false,   // show/hide Loader
+//     modalImage: '',
+//   };
+
+//   componentDidUpdate(_, prevState) {
+//     const { query, page } = this.state;
+
+//     // 
+//     if (
+//       prevState.query !== query ||
+//       prevState.page !== page) {
+      
+//       // activate preLoader;
+//       this.setState({ isLoading: true });
+
+//       // get images from server
+//       getImage({ query, page, per_page: IMAGES_PER_PAGE })
+//         .then(({ hits, totalHits }) => {
+//           this.setState(prevState => ({
+//             images: [
+//               ...prevState.images,
+//               ...hits.map(
+//                 ({ id, webformatURL, largeImageURL }) =>
+//                 ({ id, webformatURL, largeImageURL })
+//               )
+//             ],
+//             isLastPage: (page * IMAGES_PER_PAGE >= totalHits),
+//           }));
+//         })
+//         .catch(error => console.log(error))
+//         .finally(() =>  // de-activate preLoader;
+//           this.setState({ isLoading: false, })
+//         );
+//     };
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const { value } = e.currentTarget.query;
+
+//     const query = value.trim().toLowerCase();;
+//     if (!query || query === this.state.query) {
+//       return;
+//     };
+
+//     this.setState({ query, images: [], page: 1,});
+//   };
+
+//   handleLoadMore = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }))
+//   };
+
+//   toggleModal = image =>
+//     this.setState({
+//       modalImage: image ?? '',
+//     });
+
+//   render() {
+//     const { images, isLoading, isLastPage, modalImage } = this.state;
+    
+//     return (
+//       <Main>
+//         <Searchbar onSubmit={this.handleSubmit} />
+
+//         {!!images.length &&
+//           <ImageGallery images={images} toggleModal={this.toggleModal} />}
+        
+//         {isLoading && <Loader />}
+        
+//         {!!images.length &&
+//           !isLoading &&
+//           !isLastPage &&
+//           <Button onLoadMore={this.handleLoadMore} />}
+        
+//         {modalImage &&
+//           <Modal
+//             image={modalImage}
+//             toggleModal={this.toggleModal} />}
+//       </Main>
+//     );
+//   }
+// };
 
 export default App;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,37 +1,33 @@
-import { Component } from "react";
+import { useEffect } from "react";
 import { BackDrop, FaceDrop } from './Modal.styled';
 import { PropTypes } from 'prop-types';
 
 const ESC_KEY = 27;
 
-class Modal extends Component {
-    
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown);
-    };
-
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyDown);
-    };
-
-    handleKeyDown = e => {
-        if (e.keyCode === ESC_KEY) {
-            this.props.toggleModal();
+const Modal = ({ image, toggleModal }) => {
+    useEffect(() => {
+        const handleKeyDown = e => {
+            if (e.keyCode === ESC_KEY) {
+                toggleModal();
+            };
         };
-    };
 
-    render() {
-        const { image, toggleModal } = this.props;
+        window.addEventListener('keydown', handleKeyDown);
 
-        return (
-            <BackDrop onClick={() => toggleModal()}>
-                <FaceDrop>
-                    <img src={image} alt="" />
-                </FaceDrop>
-            </BackDrop>
-        );
-    }
-}
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        }
+    });
+    
+
+    return (
+        <BackDrop onClick={() => toggleModal()}>
+            <FaceDrop>
+                <img src={image} alt="" />
+            </FaceDrop>
+        </BackDrop>
+    );
+};
 
 Modal.propType = {
     image: PropTypes.string.isRequired,
